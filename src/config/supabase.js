7c2e96c198d2543b7d71bf5aa44d6e99d2703a1a
El condor SGI <sgielcondor@gmail.com>
2026-04-06 00:56:07 -0500

    Delete src directory

--- src/config/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import 'dotenv/config'
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Faltan SUPABASE_URL o SUPABASE_KEY en el archivo .env')
-}
-export const supabase = createClient(supabaseUrl, supabaseKey)
